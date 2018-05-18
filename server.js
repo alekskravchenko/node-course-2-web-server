c026aec7496bfbaf341a219e4642e30e38c1e21c
Aleks Kravchenko <web.kravchenko@gmail.com>
2018-05-18 13:48:05 +0300

    Add project page

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ var app = express();
 
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
-app.use((req, res, next) =>{
+/*app.use((req, res, next) =>{
    res.render('maintenance.hbs', {
        pageTitle: 'Maintenance',
        msg: "we will back soon"
    })
-});
+});*/
 
 hbs.registerPartials(__dirname +'/views/partials');
 hbs.registerHelper('getCurrentYear', () => {
@@ -30,9 +30,16 @@ app.get('/', (req, res)=>{
 app.get('/about', (req, res)=>{
     res.render('about.hbs', {
         pageTitle: 'About',
-        year: new Date().getFullYear()
     });
 });
+
+app.get('/projects', (req, res)=>{
+    res.render('projects.hbs', {
+        pageTitle: 'Projects',
+        message: 'This s my portfolio'
+    });
+});
+
 app.get('/bad', (req, res) =>{
    res.send({
        err: `Can't get some data`
